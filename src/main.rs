c42eb547155b05e9a663a3a3e4b3efda287a0ddc
firedestroyer53 <firedestroyer53@gmail.com>
2023-03-10 18:51:11 -0500

    evenorodd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter a number:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let num = input.trim().parse::<i64>().unwrap();
+
+    if evenorodd(num){
+        println!("{} is even", num);
+    }
+    else{
+        println!("{} is odd", num)
+    }
+}
+
+fn evenorodd(x: i64) -> bool {
+    if x%2 == 0 {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
