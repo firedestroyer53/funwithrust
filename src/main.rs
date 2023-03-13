6f3e4ecf5544d00758e491dabc1d7badc353902e
firedestroyer53 <firedestroyer53@gmail.com>
2023-03-12 20:49:59 -0400

    yea

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use rand::prelude::*;
+
 fn main(){
-    println!("Hello World");
+    let mut rng = rand::thread_rng();
+    let x:i64 = rng.gen_range(1..10000000000);
+    println!("{}", x);
+    println!("{}", evenorodd(x));
 }
 
-fn evenorodd(num: i32) -> String{
+fn evenorodd(num: i64) -> String{
     if num % 2 == 0{
         return "Even".to_string();
     }
