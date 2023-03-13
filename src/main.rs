d31549b2ce5eae0039ffdcefc34f368b342a322b
firedestroyer53 <firedestroyer53@gmail.com>
2023-03-12 22:30:16 -0400

    kslgaj

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-use rand::prelude::*;
+
 
 fn main(){
-    let mut rng = rand::thread_rng();
-    let x:i64 = rng.gen_range(1..10000000000);
-    println!("{}", x);
-    println!("{}", evenorodd(x));
-}
-
-fn evenorodd(num: i64) -> String{
-    if num % 2 == 0{
-        return "Even".to_string();
-    }
-    else{
-        return "Odd".to_string();
-    }
+    println!("Die");
 }
